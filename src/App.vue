8f9c80986b5ee2d993db976f3564d04f8515d605
missahrizona <princessssahri@gmail.com>
2022-03-02 02:03:48 -0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-app>
+		<v-app-bar app color="secondary" dark>
+			<div class="d-flex align-center">
+				<v-img
+					alt="Vuetify Logo"
+					class="shrink mr-2"
+					contain
+					src="https://www.programmableweb.com/sites/default/files/TheStarWarsAPI.png"
+					transition="scale-transition"
+					width="40"
+				/>
+
+				<v-img
+					alt="Vuetify Name"
+					class="shrink mt-1 hidden-sm-and-down"
+					contain
+					min-width="100"
+					src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+					width="100"
+				/>
+			</div>
+
+			<v-spacer></v-spacer>
+		</v-app-bar>
+
+		<v-main>
+			<HelloWorld />
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+	name: 'App',
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+	components: {
+		HelloWorld,
+	},
+
+	data: () => ({
+		//
+	}),
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,94 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<v-data-table
+		fixed-header
+		disable-pagination="true"
+		:headers="headers"
+		:items="planets"
+		:single-expand="singleExpand"
+		:expanded.sync="expanded"
+		@item-expanded="getResidentsForPlanet"
+		item-key="name"
+		show-expand
+		class="elevation-1"
+		height="80vh"
+		style="padding: 15px"
+	>
+		<template v-slot:top>
+			<v-toolbar flat>
+				<v-toolbar-title>All Planets</v-toolbar-title>
+			</v-toolbar>
+		</template>
+
+		<template v-slot:expanded-item="{ item }">
+			<v-card
+				class="mx-auto"
+				max-width="300"
+				style="margin: 25px; margin-left: 25px !important"
+				tile
+			>
+				<v-list dense>
+					<v-subheader>Residents</v-subheader>
+					<v-list-item-group color="primary">
+						<v-list-item
+							v-for="resident in item.residents"
+							v-bind:key="resident.name"
+						>
+							<v-list-item-icon>
+								<v-icon v-text="'mdi-account'"></v-icon>
+							</v-list-item-icon>
+							<v-list-item-content>
+								<v-list-item-title
+									v-text="resident.name"
+								></v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+					</v-list-item-group>
+				</v-list>
+			</v-card>
+		</template>
+	</v-data-table>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+	data() {
+		const planets = [];
+		axios
+			.get(
+				'https://star-wars--dotnet-api.azurewebsites.net/Home/GetPlanetsRaw'
+			)
+			.then((res) => {
+				this.planets = res.data.planets;
+			});
+		return {
+			expanded: [],
+			singleExpand: false,
+			headers: [
+				{
+					text: 'Name',
+					align: 'start',
+					sortable: true,
+					value: 'name',
+				},
+				{ text: 'Population', value: 'formattedPopulation' },
+				{ text: 'Diameter', value: 'diameter' },
+				{ text: 'Terrain', value: 'terrain' },
+				{ text: 'Length Of Year', value: 'lengthOfYear' },
+			],
+			planets: planets,
+		};
+	},
+	methods: {
+		getResidentsForPlanet({ item }) {
+			let uri =
+				'https://star-wars--dotnet-api.azurewebsites.net/home/GetResidentsRaw?planetname=' +
+				item.name;
+			axios.get(uri).then((res) => {
+				item.residents = res.data;
+				console.log(item);
+			});
+		},
+	},
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
